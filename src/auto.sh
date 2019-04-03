4e0efe95525e281d69c507d2b8aaecdd08b68f8c
Mike Field <hamster@snap.net.nz>
2019-04-03 22:37:06 +1300

    Enhance auto.sh to error out if build fails

--- src/auto.sh
+++ src/auto.sh
@@ -5,7 +5,18 @@ do
 	if [ -d $dir ]
 	then
 		echo $dir
+		rm -f $dir/$dir.bin
+
+		if [ ! -d $dir/build/ ]
+		then
+			mkdir $dir/build
+		fi
 		cd $dir/build/ && cmake ../../../ && make && cp $dir.bin ../ && cd -
+		if [ ! -f $dir/$dir.bin ]
+		then
+			echo ERROR: Unable to build $dir
+			exit
+		fi
 	fi
 done
 set +e
